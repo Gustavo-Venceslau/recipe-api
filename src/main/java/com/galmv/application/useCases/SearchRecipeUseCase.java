3d181e264d9ff9d5a4219658089cb36a8d9ae2ef
Gustavo-de-Almeida <gustavodealmeida01@gmail.com>
2025-06-23 08:00:37 -0300

    refactor search recipe use case

--- src/main/java/com/galmv/application/useCases/SearchRecipeUseCase.java
+++ src/main/java/com/galmv/application/useCases/SearchRecipeUseCase.java
@@ -19,35 +19,54 @@ public class SearchRecipeUseCase {
   public Set<Recipe> execute(SearchRecipesFilter filter) {
     Specification<Recipe> spec = Specification.anyOf();
 
+    spec = addVegetarianFilter(spec, filter);
+    spec = addServingsFilter(spec, filter);
+    spec = addIncludeIngredientsFilter(spec, filter);
+    spec = addExcludeIngredientsFilter(spec, filter);
+    spec = addInstructionsFilter(spec, filter);
+
+    return Set.copyOf(recipeRepository.findAll(spec));
+  }
+
+  private Specification<Recipe> addVegetarianFilter(Specification<Recipe> spec, SearchRecipesFilter filter) {
     if (filter.vegetarian() != null) {
-      spec = spec.and((root, query, cb) ->
-        cb.equal(root.get("vegetarian"), filter.vegetarian()));
+      return spec.and((root, query, cb) -> cb.equal(root.get("vegetarian"), filter.vegetarian()));
     }
+    return spec;
+  }
 
+  private Specification<Recipe> addServingsFilter(Specification<Recipe> spec, SearchRecipesFilter filter) {
     if (filter.servings() != null) {
-      spec = spec.and((root, query, cb) ->
-        cb.equal(root.get("servings"), filter.vegetarian()));
+      return spec.and((root, query, cb) -> cb.equal(root.get("servings"), filter.servings()));
     }
+    return spec;
+  }
 
+  private Specification<Recipe> addIncludeIngredientsFilter(Specification<Recipe> spec, SearchRecipesFilter filter) {
     if (filter.includeIngredients() != null && !filter.includeIngredients().isEmpty()) {
       for (String ingridient : filter.includeIngredients()) {
         spec = spec.and((root, query, cb) ->
-          cb.isMember(ingridient, root.get("ingridients")));
+          cb.isMember(ingridient, root.get("ingredients")));
       };
     }
+    return spec;
+  }
 
+  private Specification<Recipe> addExcludeIngredientsFilter(Specification<Recipe> spec, SearchRecipesFilter filter) {
     if (filter.excludeIngredients() != null && !filter.excludeIngredients().isEmpty()) {
       for (String ingridient : filter.excludeIngredients()) {
         spec = spec.and((root, query, cb) ->
-          cb.not(cb.isMember(ingridient, root.get("ingridients"))));
+          cb.not(cb.isMember(ingridient, root.get("ingredients"))));
       };
     }
+    return spec;
+  }
 
+  private Specification<Recipe> addInstructionsFilter(Specification<Recipe> spec, SearchRecipesFilter filter) {
     if(filter.instructions() != null && !filter.instructions().isBlank()) {
-      spec = spec.and((root, query, cb) ->
+      return spec.and((root, query, cb) ->
         cb.like(root.get("instructions"), "%" + filter.instructions() + "%"));
     }
-
-    return Set.copyOf(recipeRepository.findAll(spec));
+    return spec;
   }
 }
